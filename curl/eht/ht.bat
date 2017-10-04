65e3de04c232538624ea71d39d4728a48f07fba5
gbm618 <gbm618@gmail.com>
2017-10-04 08:45:36 +0800

    v1

--- curl/eht/ht.bat
+++ curl/eht/ht.bat
@@ -1,2 +1,19 @@
 #!/bin/bash
-curl 
+addr2=""
+addr1=""
+while [ "$addr1" != "$addr2" ]
+do
+addr1=$addr2
+curl -k -o o100.html $addr1
+sed 's/</\n</g' o100.html | grep -i keystamp > o101.html
+sed 's/http/\nhttp/g' o101.html >o102.html
+sed 's/\.jpg/\.jpg\n/g' o102.html > o103.html
+fn=$(grep http o103.html)
+echo $fn
+ofn=$(grep http o103.html | sed 's/\//\n/g' | grep -i '\.jpg')
+echo $ofn
+curl -o $ofn $fn
+sed 's/</\n</g' o100.html | grep -i next | sort -u > o201.html
+addr2=$(sed 's/https/\nhttps/g' o201.html | grep http | sed 's/\"/\n/g' | grep http)
+echo $addr2
+done
